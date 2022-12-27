2d9538540c70848cbc483cb6b024778401a925dd
Eakkarin06 <prc36516@gmail.com>
2022-12-27 13:51:34 +0700

    a

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 
+using #include<iostream>
+
 using namespace std;
 
 int main(){
---
+++ lab7_3.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+  #include<iostream>
+
+using namespace std;
+
+int adiff(int x1,int x2)
+{
+  int r1,r2;
+  r1 = x1%360;
+  r2 = x2%360;
+  if(r1>r2)
+  {
+  if( (r1-r2) <=180 )
+  {
+    return (r1-r2);
+  }
+  else if ( (r1-r2) >180)
+  {
+    return (360-(r1-r2));
+  }
+  }
+  else
+  {
+    if( (r2-r1) <=180 )
+  {
+    return (r2-r1);
+  }
+  else if ( (r2-r1) >180)
+  {
+    return (360-(r2-r1));
+  }
+  }
+  
+  return 0;
+}
+
